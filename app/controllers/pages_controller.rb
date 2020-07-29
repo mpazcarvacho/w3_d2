17c2c41e58216fc4ab49595071cafb428be68c8d
mariapaz <mpazcarvacho@gmail.com>
2020-07-29 18:32:16 -0400

    añadidas pages login, about us y links en nav bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,11 @@ class PagesController < ApplicationController
         Rails.logger.info("##########################################################################################")
         Rails.logger.info("##########################################################################################")
     end
+
     def test
     end
+
+    def about_us
+    end
+
 end
