3dd984e4c724331ed838c7ff67730bc63fd4ba9c
mariapaz <mpazcarvacho@gmail.com>
2020-07-29 17:57:31 -0400

    template added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,17 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+
+
+//= require jquery.min.js
+//= require jquery.dropotron.min.js
+//= require jquery.scrolly.min.js
+//= require jquery.scrollex.min.js
+//= require browser.min.js
+//= require breakpoints.min.js
+//= require util.js
+//= require main.js
+
 //= require_tree .
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  #THIS IS NEW
+  config.serve_static_assets = false
+  config.assets.digest = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,7 +27,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
